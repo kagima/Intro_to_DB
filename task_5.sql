6147d0d6c2d03ea54cc7c7177761a7746b2c9d7f
chrispus <kagimachris@gmail.com>
2025-08-16 19:05:15 +0300

    a script that inserts a single row

---
+++ task_5.sql
@@ -0,0 +1,6 @@
+USE alx_book_store;
+
+INSERT INTO customers (customer_id, customer_name, email, address) 
+VALUES (1, 'Cole joe','cbaidoo@sandtech.com','456 Happiness Ave.');
+
+SELECT * FROM customers;
